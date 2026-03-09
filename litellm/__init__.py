f8389516873a091e67ac0658cfb3aa1228a8002c
Jia Jia <angersax@sina.com>
2026-03-09 03:11:10 +0000

    feat(provider): add llm providers

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -597,6 +597,8 @@ aws_polly_models: Set = set()
 gigachat_models: Set = set()
 llamagate_models: Set = set()
 bedrock_mantle_models: Set = set()
+nebulacoder_models: Set = set()
+siliconflow_models: Set = set()
 
 
 def is_bedrock_pricing_only_model(key: str) -> bool:
@@ -861,6 +863,10 @@ def add_known_models(model_cost_map: Optional[Dict] = None):
             llamagate_models.add(key)
         elif value.get("litellm_provider") == "bedrock_mantle":
             bedrock_mantle_models.add(key)
+        elif value.get("litellm_provider") == "nebulacoder":
+            nebulacoder_models.append(key)
+        elif value.get("litellm_provider") == "siliconflow":
+            siliconflow_models.append(key)
 
 
 add_known_models()
@@ -970,6 +976,8 @@ model_list = list(
     | docker_model_runner_models
     | bedrock_mantle_models
     | set(clarifai_models)
+    | nebulacoder_models
+    | siliconflow_models
 )
 
 model_list_set = set(model_list)
@@ -1072,7 +1080,9 @@ models_by_provider: dict = {
     "aws_polly": aws_polly_models,
     "gigachat": gigachat_models,
     "llamagate": llamagate_models,
-    "bedrock_mantle": bedrock_mantle_models
+    "bedrock_mantle": bedrock_mantle_models,
+    "nebulacoder": nebulacoder_models,
+    "siliconflow": siliconflow_models,
 }
 
 # mapping for those models which have larger equivalents
@@ -1559,6 +1569,12 @@ if TYPE_CHECKING:
     from .llms.lemonade.chat.transformation import LemonadeChatConfig as LemonadeChatConfig
     from .llms.snowflake.embedding.transformation import SnowflakeEmbeddingConfig as SnowflakeEmbeddingConfig
     from .llms.amazon_nova.chat.transformation import AmazonNovaChatConfig as AmazonNovaChatConfig
+    from .llms.nebulacoder.chat.transformation import NebulaCoderChatConfig
+    from .llms.nebulacoder.embedding.transformation import NebulaCoderEmbeddingConfig
+    from .llms.nebulacoder.rerank.transformation import NebulaCoderRerankConfig
+    from .llms.siliconflow.chat.transformation import SiliconFlowChatConfig
+    from .llms.siliconflow.embedding.transformation import SiliconFlowEmbeddingConfig
+    from .llms.siliconflow.rerank.transformation import SiliconFlowRerankConfig
     from litellm.caching.llm_caching_handler import LLMClientCache
     from litellm.types.llms.bedrock import COHERE_EMBEDDING_INPUT_TYPES
     from litellm.types.utils import (
